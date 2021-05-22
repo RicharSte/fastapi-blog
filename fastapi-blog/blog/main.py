27ad1985614b97aeb8929b54c256a99e56e94cf9
Nikolai Stepanov <nikolaistepanov@MacBook-Air-Nikolai.local>
2021-05-22 22:23:55 +0300

    Add 1/2 Blog

---
+++ fastapi-blog/blog/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Depends, Response, status
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+from .schemas import Blog
+from .database import engine, SessionLocal
+from . import models
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}', status_code=200)
+def show(id, response:Response ,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail={'detail': f'{id} is not avalible'})
+    
+    return blog
