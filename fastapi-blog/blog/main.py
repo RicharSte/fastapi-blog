fe91115e111c2e16dd719d47035cdc98e3d3947c
Nikolai Stepanov <nikolaistepanov@MacBook-Air-Nikolai.local>
2021-05-22 23:47:41 +0300

    Puth method is added

--- fastapi-blog/blog/main.py
+++ fastapi-blog/blog/main.py
@@ -5,7 +5,6 @@ from .schemas import Blog
 from .database import engine, SessionLocal
 from . import models
 
-
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
@@ -24,6 +23,26 @@ def create(request: Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id, db: Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f('Blog {id} ist not found'))
+    
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f('Blog {id} ist not found'))
+    blog.update(request.dict())
+    db.commit()
+    
+    return 'updated'
 
 @app.get('/blog')
 def get_all_blogs(db: Session = Depends(get_db)):
